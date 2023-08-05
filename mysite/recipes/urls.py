99da77a0365713d9cd53b450a8927becb802da09
Luke Moran <luke.moran2004@gmail.com>
2023-08-05 23:40:23 +0100

    Added home page with cards indicating features. Made the buttons on the navbar link to actual pages and added template in to add the meal tracker later.

--- mysite/recipes/urls.py
+++ mysite/recipes/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 app_name = "recipes"
 urlpatterns = [
-    path('', views.recipe_list),
-    path("recipe_details/<int:recipe_id>/", views.recipe_detail, name="recipe_detail")
+    path('', views.index, name="home"),
+    path('recipes', views.recipe_list, name="recipe_list"),
+    path("recipe_details/<int:recipe_id>/", views.recipe_detail, name="recipe_detail"),
+    path("meal-tracker", views.meal_planner, name="meal_planner_home")
 ]
--- mysite/recipes/views.py
+++ mysite/recipes/views.py
@@ -4,16 +4,18 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Recipe, RecipeIngredient
 
+
 def recipe_list(request):
 
     recipes = Recipe.objects.all()
     rdetails = {}
     for recipe in recipes:
         #print(recipe.name, "HERE WITH ID", recipe.id)
-        res = RecipeIngredient.objects.all()
+        res = RecipeIngredient.objects.filter(rname=recipe.id)
         # for i in res:
         #     print("FOUND Ingredient", i.rname, i.iname, i.recipeQuantityGrams)
         rdetails[recipe] = res
+        #print(res, recipe.name)
         
     context = {"recipes":recipes, "rdetails":rdetails}
 
@@ -22,3 +24,9 @@ def recipe_list(request):
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
     return render(request, "recipes/recipe_detail.html", {"recipe":recipe})
+
+def index(request):
+    return render(request, "recipes/index.html")
+
+def meal_planner(request):
+    return render(request, "BLAH")
