1f0a0f656ddd593722b34565c4c41cd245654059
Luke Moran <luke.moran2004@gmail.com>
2023-08-08 11:00:15 +0100

    Fixed login and registration forms and display errors on registration

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
 # Create your views here.
 
 
 def login(request):
     if request.method == "POST":
-        print("POST ATTEMPTED")
+
+        user = authenticate(request, username=request.POST["username"], password=request.POST["password"])
+
+        if user is None:
+            context = {"error":"Invalid username or password"}
+            return render(request, "accounts/login.html", context)
+
+        
         return redirect("index")
     return render(request, "accounts/login.html")
 def register(request):
-    print(request, "HERE", request.POST)
-
+    #print(request, "HERE", request.POST)
+    if request.user.is_authenticated:
+        return redirect("index")
+    
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, "Account created succesfully.")
-        return redirect("index")
+            
+            return redirect("index")
+        ### Failed to register user
+        print(form.errors.as_text)
+        context = {"errors": form.errors.as_text}
+        return render(request, "accounts/register.html", context)
     return render(request, "accounts/register.html")
